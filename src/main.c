32d981f992b18fd399ad5dcf85b05c059ad31a1c
zzzzzzzs <1443539042@qq.com>
2022-05-14 09:01:14 +0800

    Simplest SQL Compiler and Virtual Machine

--- src/main.c
+++ src/main.c
@@ -6,8 +6,8 @@
 #include <string.h>
 #include <sys/types.h>
 
-#include "sds.h"
 #include "print.h"
+#include "sds.h"
 
 void sigintHandler(int sig) {
   if (sig == SIGINT) {
@@ -17,6 +17,21 @@ void sigintHandler(int sig) {
     printLeaks();
   }
 }
+
+// meta command
+typedef enum {
+  META_COMMAND_SUCCESS,
+  META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT } PrepareResult;
+
+// prepared statement
+typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
+
+typedef struct {
+  StatementType type
+} Statement;
 
 typedef struct {
   char* buffer;
@@ -35,7 +50,7 @@ InputBuffer* newInputBuffer() {
 
 void printPrompt() { printf("db > "); }
 
-void readInput(sds *inputBuffer) {
+void readInput(sds* inputBuffer) {
   int len;
   ssize_t bytesRead = getline(inputBuffer, &len, stdin);
 
@@ -43,11 +58,8 @@ void readInput(sds *inputBuffer) {
     printf("Error reading input\n");
     exit(EXIT_FAILURE);
   }
-  DBG_PRINTF(*inputBuffer);
-
-  // Ignore trailing newline
-  // inputBuffer->inputLength = bytesRead - 1;
-  // inputBuffer->buffer[bytesRead - 1] = 0;
+  // Remove the last line
+  strtok(*inputBuffer, "\n");
 }
 
 void close_input_buffer(InputBuffer* input_buffer) {
@@ -55,19 +67,70 @@ void close_input_buffer(InputBuffer* input_buffer) {
   free(input_buffer);
 }
 
+// check meta-commands
+MetaCommandResult doMetaCommand(sds* inputBuffer) {
+  if (strcmp(*inputBuffer, ".exit") == 0) {
+    printf("program is success exit.\n");
+    exit(EXIT_SUCCESS);
+  } else {
+    return META_COMMAND_UNRECOGNIZED_COMMAND;
+  }
+}
+
+// SQL Compiler
+PrepareResult prepareStatement(sds* inputBuffer, Statement* statement) {
+  if (strncmp(*inputBuffer, "insert", 6) == 0) {
+    statement->type = STATEMENT_INSERT;
+    return PREPARE_SUCCESS;
+  }
+  if (strcmp(*inputBuffer, "select") == 0) {
+    statement->type = STATEMENT_SELECT;
+    return PREPARE_SUCCESS;
+  }
+
+  return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+// execute sql
+void executeStatement(Statement* statement) {
+  switch (statement->type) {
+    case (STATEMENT_INSERT):
+      printf("This is where we would do an insert.\n");
+      break;
+    case (STATEMENT_SELECT):
+      printf("This is where we would do a select.\n");
+      break;
+  }
+}
+
 int main(int argc, char* argv[]) {
   signal(SIGINT, sigintHandler);
   sds inputBuffer = NULL;
-  
+
+  // loop
   while (true) {
     printPrompt();
     readInput(&inputBuffer);
-    DBG_PRINTF(inputBuffer);
-    if (strcmp(inputBuffer, ".exit") == 0) {
-      close_input_buffer(inputBuffer);
-      exit(EXIT_SUCCESS);
-    } else {
-      printf("Unrecognized command '%s'.\n", inputBuffer);
+
+    // meta command
+    if (inputBuffer[0] == '.') {
+      switch (doMetaCommand(&inputBuffer)) {
+        case (META_COMMAND_SUCCESS):
+          continue;
+        case (META_COMMAND_UNRECOGNIZED_COMMAND):
+          printf("Unrecognized command '%s' \n", inputBuffer);
+          continue;
+      }
     }
+    Statement statement;
+    switch (prepareStatement(&inputBuffer, &statement)) {
+      case (PREPARE_SUCCESS):
+        break;
+      case (PREPARE_UNRECOGNIZED_STATEMENT):
+        printf("Unrecognized keyword at start of '%s'.\n", inputBuffer);
+        continue;
+    }
+    executeStatement(&statement);
+    printf("Executed.\n");
   }
 }
